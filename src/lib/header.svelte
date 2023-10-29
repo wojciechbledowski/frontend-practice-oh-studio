a8755720877636c293b17a493bcee237685bd6b1
Wojciech Błędowski <wojciech@solutions4delivery.com>
2023-10-29 21:02:42 +0100

    Fixed static page routing missing base path

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,26 +1,27 @@
 <script>
 	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 	$: routeId = $page.route.id;
 </script>
 
 <nav class="sticky top-0 py-15 flex justify-center">
 	<div class="flex gap-2 bg-light-gray/50 backdrop-blur-lg p-2 rounded-full w-[300px]">
 		<a
-			href="/"
+			href="{base}/"
 			class="flex justify-center items-center rounded-full grow h-20"
 			class:bg-white={routeId === '/'}
 		>
 			<p class="font-montreal text-sm">Home</p>
 		</a>
 		<a
-			href="/profile"
+			href="{base}/profile"
 			class="flex justify-center items-center bg-white rounded-full grow h-20"
 			class:bg-white={routeId === '/profile'}
 		>
 			<p class="font-montreal text-sm">Profile</p>
 		</a>
 		<a
-			href="/contact"
+			href="{base}/contact"
 			class="flex justify-center items-center bg-white rounded-full grow h-20"
 			class:bg-white={routeId === '/contact'}
 		>
