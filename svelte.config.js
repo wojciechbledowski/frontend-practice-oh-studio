abd6bb80b224d06c16c67d70d1b71bff80a7b49f
Wojciech Błędowski <wojciech@solutions4delivery.com>
2023-11-07 21:55:39 +0100

    Changed production path

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/frontend-practice-oh-studio-github-pages' : ''
+			base: process.env.NODE_ENV === 'production' ? '/frontend-practice-oh-studio' : ''
 		}
 	}
 };
